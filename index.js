f66d7a2b4a99f5dd835e2c3ff82fe5d574aef7cf
nazzout <natalie.peckham@medlmobile.com>
2024-06-03 21:57:05 -0700

    added more complexity to the form

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
 const modal = document.getElementById('modal');
 const modalCloseBtn = document.getElementById('modal-close-btn');
+const declineBtn = document.getElementById('decline-btn');
 
 setTimeout(function() {
     modal.style.display = 'inline';
     }, 1500);
 
-modalCloseBtn.addEventListener('click', function() {
+    modalCloseBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    declineBtn.addEventListener('click', function() {
         modal.style.display = 'none';
     });
