b8c80961744fdb7b23e04f66d2f5b1f2bba382a4
nazzout <natalie.peckham@medlmobile.com>
2024-06-04 21:23:18 -0700

    updated form
    added modal message, submitting and preventDefault

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
+document.addEventListener('DOMContentLoaded', function() {
 const modal = document.getElementById('modal');
 const modalCloseBtn = document.getElementById('modal-close-btn');
+const modalText = document.getElementById('modal-text');
+const consentForm = document.getElementById('consent-form');
 const declineBtn = document.getElementById('decline-btn');
 
 setTimeout(function() {
@@ -11,6 +14,28 @@ setTimeout(function() {
         modal.style.display = 'none';
     });
 
+    consentForm.addEventListener('submit', function(e) {
+        e.preventDefault(); // Prevent the default form submission
+        modalText.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading" alt="Loading">
+            <p id="uploadText">Uploading your data to the dark web...</p>
+        </div>
+        `;
+        console.log('form submitted');
+    });
+
+
     declineBtn.addEventListener('click', function() {
         modal.style.display = 'none';
     });
+
+});
+    /*
+Challenge:
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+   the accept button, the HTML string below
+   is inserted into the modal-text div.*/
+
+
